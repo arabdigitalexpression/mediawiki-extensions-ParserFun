080d736729ffbdc9595083e6ede169f3408c1339
Ahmad Gharbeia <gharbeia@gmail.com>
2018-11-03 12:06:39 +0000

    Removed code-based Validator dependency
    Removed code that check for the existence of Extension:Validator and loads it.

--- ParserFun.php
+++ ParserFun.php
@@ -20,16 +20,6 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
 }
 
-// Include the Validator extension if not loaded already:
-if ( ! defined( 'Validator_VERSION' ) && file_exists( dirname( __FILE__ ) . '/../Validator/Validator.php' ) ) {
-	include_once( dirname( __FILE__ ) . '/../Validator/Validator.php' );
-}
-
-// Only initialize the extension when Validator extension is present:
-if ( ! defined( 'Validator_VERSION' ) ) {
-	die( '<p><b>Error:</b> You need to have <a href="https://www.mediawiki.org/wiki/Extension:Validator">Validator</a> installed in order to use <a href="https://www.mediawiki.org/wiki/Extension:ParserFun">ParserFun</a>.</p>' );
-}
-
 /**
  * Extension class of the 'Parser Fun' extension.
  * Handling the functionality around the 'THIS' magic word feature.
